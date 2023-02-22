7e87b3c8f256395300eeeca6be634f4ee5e5c89a
Ismail Baraldini <romeo.baraldini@gmail.com>
2023-02-22 17:10:10 +0300

    Start Project

---
+++ src/main/java/com/example/batflix/BatflixApplication.java
@@ -0,0 +1,13 @@
+package com.example.batflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/batflix/BatflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
